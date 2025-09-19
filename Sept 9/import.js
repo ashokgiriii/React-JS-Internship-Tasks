f45d00afde7744a4b2b54355dea31136fa89238c
Ashok Giri <sautaboy@gmail.com>
2025-09-19 10:22:29 +0545

    loader added

--- Sept 9/import.js
+++ Sept 9/import.js
@@ -4,10 +4,16 @@ import { fetchData } from "./export.js";
 const container = document.getElementById("container");
 
 fetchData().then((data) => {
+  if (!data) {
+    container.innerHTML = `
+    <h1>Loading</h1>
+    `;
+    return;
+  }
   data.forEach((eachdata) => {
     container.innerHTML += `
      <li>
-        <span class="id">${eachdata.id} :  </span>
+        <span class="id">${eachdata.id} : </span>
         <span class="title">${eachdata.title}</span>
       </li>
       `;
